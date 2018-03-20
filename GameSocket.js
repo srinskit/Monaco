0b252d7d864a1d8a9c276ffa16088a5f02ef3998
srinskit <srinagrao2007@gmail.com>
2018-03-20 15:33:45 +0530

    Added Bingo Game Class

--- GameSocket.js
+++ GameSocket.js
@@ -2,50 +2,263 @@ const WebSocketServer = require('ws').Server;
 const Url = require('url');
 const AUTH = 'auth';
 
-class GameClient {
-    constructor(wsClient, username) {
-        this.wsClient = wsClient;
-        this.username = username;
-        this.wsClient.on('message', this.onMessage);
-        this.wsClient.on('close', this.onClose);
-        this.wsClient.on('error', this.onError);
+class Game {
+    constructor(gClient0, gClient1) {
+        this.gClients = [gClient0, gClient1];
+        this.grids = [undefined, undefined];
+        this.reverseMap = [undefined, undefined];
+        this.points = [0, 0];
+        this.gameReady = false;
+        this.whoseTurn = Math.round(Math.random());
+        this.notifyAll(GameSocket.makeJsonMsg('notify', {
+            msg: 'gameConstructed',
+            turn: this.gClients[this.whoseTurn].username
+        }));
     }
 
-    onMessage(msg) {
-        GameSocket.exec(JSON.parse(msg), this);
+    static validateGrid(grid) {
+        return true;
     }
 
-    onClose() {
-
+    static generateReverseMap(grid) {
+        let map = new Array(26);
+        for (let i = 1; i <= 5; ++i)
+            for (let j = 0; j <= 5; ++j)
+                map[grid[i][j]] = [i, j];
+        // Return let obj problem much?
+        return map;
     }
 
-    onError() {
-
+    initGrid(gClient, grid) {
+        if (!Game.validateGrid(grid))
+            return;
+        let id = this.gClients.indexOf(gClient);
+        if (id === -1 || this.grids[id] !== undefined)
+            return;
+        this.grids[id] = grid;
+        this.reverseMap[id] = Game.generateReverseMap(grid);
+        if (this.grids[0] !== undefined && this.grids[1] !== undefined) {
+            this.notifyAll(GameSocket.makeJsonMsg('notify', {msg: 'gameReady'}));
+            this.gameReady = true;
+        }
     }
 
-    send(msg, callback) {
-        if (callback)
-            this.wsClient.send(msg, callback);
-        else
-            this.wsClient.send(msg);
+    makeMove(gClient, num) {
+        num = Math.round(num);
+        if (!(1 <= num <= 25) || this.gameReady !== true) return;
+        if (this.gClients[this.whoseTurn].username !== gClient.username) return;
+        let coordinates = this.reverseMap[this.whoseTurn][num];
+        let i = coordinates[0], j = coordinates[1], id = this.whoseTurn;
+        if (this.grids[id][i][j] === 0) return;
+        this.grids[id][i][j] = 0;
+        let k;
+        if (i === j) {
+            for (k = 0; k < 5; ++k)
+                if (this.grids[id][k][k] !== 0)
+                    break;
+            if (k === 5) this.points[id]++;
+        }
+        if (i === 5 - 1 - j) {
+            for (k = 0; k < 5; ++k)
+                if (this.grids[id][k][5 - 1 - k] !== 0)
+                    break;
+            if (k === 5) this.points[id]++;
+        }
+        for (k = 0; k < 5; ++k)
+            if (this.grids[id][i][k] !== 0)
+                break;
+        if (k === 5) this.points[id]++;
+        for (k = 0; k < 5; ++k)
+            if (this.grids[id][k][j] !== 0)
+                break;
+        if (k === 5) this.points[id]++;
+        let winningMove = undefined;
+        if (this.points[id] >= 5) {
+            winningMove = true;
+            this.endGame();
+        }
+        this.notifyAll(GameSocket.makeJsonMsg('notify', {
+            msg: 'gameMove',
+            num: num,
+            winningMove: winningMove
+        }));
+        this.whoseTurn = (this.whoseTurn + 1) % 2;
     }
 
-    checkAlive(callback) {
-        this.send(GameSocket.makeStringMsg('status'));
-        callback();
+
+    endGame() {
+        this.notifyAll(GameSocket.makeJsonMsg('notify', 'gameEnd'));
+    }
+
+    notifyAll(notification) {
+        this.gClients[0].send(notification);
+        this.gClients[1].send(notification);
     }
 }
 
+class GameClient {
+
+    constructor(wsClient, username) {
+        this.wsClient = wsClient;
+        wsClient.gClient = this;
+        this.username = username;
+        GameClient.users[username] = this;
+        wsClient.on('message', GameClient.onMessage);
+        wsClient.on('close', GameClient.onClose);
+        wsClient.on('error', GameClient.onError);
+    }
+
+    static onMessage(msg) {
+        let gameClient = this.gClient;
+        GameSocket.exec(JSON.parse(msg), gameClient);
+    }
+
+    static onClose() {
+        let gameClient = this.gClient;
+        GameClient.users[gameClient.username] = undefined;
+    }
+
+    static onError() {
+        let gameClient = this.gClient;
+        GameClient.users[gameClient.username] = undefined;
+    }
+
+    send(jsonMsg, callback) {
+        if (callback)
+            this.wsClient.send(JSON.stringify(jsonMsg), callback);
+        else
+            this.wsClient.send(JSON.stringify(jsonMsg));
+    }
+}
+
+
 class Lobby {
+
     constructor(name, owner) {
         this.owner = owner;
         this.name = name;
-        this.clients = [];
+        this.members = {};
+        this.members[owner.username] = owner;
     }
 
-    addGameClient(gameClient) {
+    notifyAll(jsonMsg, callback) {
+        for (let username in this.members)
+            this.members[username].send(jsonMsg, callback);
+    }
+
+    addUser(gameClient, requestBy) {
+        if (this.members[requestBy.username] !== undefined) {
+            this.members[gameClient.username] = gameClient;
+            this.notifyAll(GameSocket.makeJsonMsg('lobbyNotify', {msg: 'userAdd', username: gameClient.username}));
+        }
+        else {
+
+        }
+    }
+
+
+    removeUser(gameClient, requestBy) {
+        if ((requestBy.username === this.owner.username || requestBy.username === gameClient.username)
+            && this.members[gameClient.username] !== undefined) {
+            this.members[gameClient.username] = undefined;
+            this.notifyAll(GameSocket.makeJsonMsg('lobbyNotify', {msg: 'userRemove', username: gameClient.username}));
+        }
+        else {
+
+        }
+    }
+
+    joinUser(gameClient) {
+        this.members[gameClient.username] = gameClient;
+        this.notifyAll(GameSocket.makeJsonMsg('lobbyNotify', {msg: 'userJoin', username: gameClient.username}));
+    }
+
+    leaveUser(gameClient) {
+        this.members[gameClient.username] = undefined;
+        this.notifyAll(GameSocket.makeJsonMsg('lobbyNotify', {msg: 'userLeave', username: gameClient.username}));
+    }
+
+    transferOwnership(gameClient, requestBy) {
+        if (requestBy.username === this.owner.username) {
+            this.owner = gameClient;
+        }
+        else {
+
+        }
+    }
+
+    destroy(requestBy) {
+        if (requestBy.username === this.owner.username) {
+
+        }
+        else {
+
+        }
+    }
+
+    broadcast(data, requestBy) {
+        if (this.members[requestBy.username] !== undefined) {
+            const msg = GameSocket.makeStringMsg('broadcast', data);
+            for (let member in this.members)
+                if (member.username !== requestBy.username)
+                    member.send(msg);
+        }
+        else {
+
+        }
+    }
+
+    static createLobby(lobbyName, requestBy) {
+        if (Lobby.lobbies[lobbyName] !== undefined) {
+            requestBy.send({type: 'err', data: {msg: 'Choose different lobby name'}});
+        }
+        else {
+            Lobby.lobbies[lobbyName] = new Lobby(lobbyName, requestBy);
+            requestBy.send({type: 'notify', data: {msg: 'Created lobby!'}});
+        }
+    }
+
+    static destroyLobby(lobbyName, requestBy) {
+        let lobby = Lobby.lobbies[lobbyName];
+        if (lobby !== undefined) {
+            lobby.destroy(requestBy);
+        }
+        else {
+
+        }
+    }
+
+    static addUserToLobby(lobbyName, gameClient, requestBy) {
+        let lobby = Lobby.lobbies[lobbyName];
+        if (lobby !== undefined) {
+            lobby.addUser(gameClient, requestBy);
+        }
+        else {
+
+        }
+    }
+
+    static broadcastToLobby(lobbyName, data, requestBy) {
+        let lobby = Lobby.lobbies[lobbyName];
+        if (lobby !== undefined) {
+            lobby.broadcast(JSON.parse(data), requestBy);
+        }
+        else {
+
+        }
+    }
+
+    static joinLobby(lobbyName, requestBy) {
+        let lobby = Lobby.lobbies[lobbyName];
+        if (lobby !== undefined) {
+            lobby.userJoin(requestBy);
+        }
+        else {
+
+        }
     }
 }
+
 
 class GameSocket {
 
@@ -54,17 +267,16 @@ class GameSocket {
             server: httpServer,
             path: pathOfSocketServer
         });
-        this.gUsers = {};
-        this.wss.on('connection', (wsClient, req) => {
-            this.onConnection(wsClient, req);
-        });
+        this.wss.on('connection', GameSocket.onConnection);
+        GameClient.users = {};
+        Lobby.lobbies = {};
+        console.log('Started Server!')
     }
 
-    onConnection(wsClient, req) {
+    static onConnection(wsClient, req) {
         let cred = GameSocket.getCred(req);
         if (GameSocket.authCred(cred) === true) {
             let gameClient = new GameClient(wsClient, cred.username);
-            this.gUsers[gameClient.username] = gameClient;
             GameSocket.onAuthPass(wsClient);
         }
         else {
@@ -104,31 +316,52 @@ class GameSocket {
         return JSON.stringify({type: type, data: data});
     }
 
+    static directMessage(data, toUsername, requestBy) {
+        let toClient = GameClient.users[toUsername];
+        if (toClient !== undefined) {
+            toClient.send(this.makeStringMsg('dm', {
+                from: requestBy.username,
+                data: data
+            }));
+        }
+        else {
+            requestBy.send(this.makeStringMsg('notify', {msg: 'dm failed'}));
+        }
+    }
+
+
     static exec(command, gameClient) {
+        if (command.data === undefined || command.type === undefined)
+            return;
         switch (command.type) {
-            case 'createLobby':
+            case 'lobbyCreate':
+                Lobby.createLobby(command.data.lobbyName, gameClient);
                 break;
-            case 'destroyLobby':
+            case 'lobbyDestroy':
+                Lobby.destroyLobby(command.data.lobbyName, gameClient);
                 break;
-            case 'joinLobby':
+            case 'lobbyAddUser':
+                Lobby.addUserToLobby(command.data.lobbyName, GameClient.users[command.data.username], gameClient);
                 break;
-            case 'leaveLobby':
+            case 'lobbyRemoveUser':
+                Lobby.addUserToLobby(command.data.lobbyName, GameClient.users[command.data.username], gameClient);
                 break;
-            case 'broadcastMsg':
+            case 'lobbyJoin':
+                Lobby.addUserToLobby(command.data.lobbyName, GameClient.users[command.data.username], gameClient);
+                break;
+            case 'lobbyLeave':
+                Lobby.addUserToLobby(command.data.lobbyName, GameClient.users[command.data.username], gameClient);
+                break;
+            case 'lobbyBroadcastMsg':
+                Lobby.broadcastToLobby(command.data.lobbyName, command.data, gameClient);
                 break;
             case 'directMsg':
+                this.directMessage(command.data.to, command.data.data, gameClient);
                 break;
         }
 
     }
 
-    createLobby() {
-
-    }
-
-    destroyLobby() {
-
-    }
 
 }
 
--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,8 +18,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.post('/', function (req, res) {
+    res.cookie('username', req.body.name, {
+        maxAge: 7200000
+    });
+    res.redirect('/play');
+});
+app.get('/play', function (req, res) {
+    res.render('play');
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
